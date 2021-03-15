2bbc778093e4a5e1bf0a4b48ecf6cbe66f5e39c9
Lera <77551820+lerastarynets@users.noreply.github.com>
2021-03-15 17:47:56 +0200

    feat:
    add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let configsArr = [];
+    let forStr = str;
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        configsArr.push(bracketsConfig[i].join(""));
+    }
+
+    for (let i = 0; i <= forStr.length; i++) {
+        for (let j = 0; j < configsArr.length; j++) {
+            if (str.includes(configsArr[j])) {
+                str = str.replace(configsArr[j], "");
+            } else {
+                continue;
+            }
+        }
+    }
+    if (str.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
